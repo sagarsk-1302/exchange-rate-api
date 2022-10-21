b96f5a7851c3d42f36ef076e7897afd8e4b7ea79
sagar-savvy <sagarsk1302@gmail.com>
2022-10-21 21:20:52 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// Post Req Body Check
+app.post("/api", function (req, res, next) {
+    if (Object.keys(req.body).length === 0) {
+        res.status(400).json({
+            Status: {
+                ResponseCode: 400,
+                ResponseMessage: "Bad Request!",
+            }
+        });
+    } else {
+        next();
+    }
+});
+
+
+
+// using the api defined in student and teacher folder
+app.use("/api/exchange-rates", require("./exchange-rates/index"))
+
+
+//test the api
+app.get("/api/", (req, res) => {
+    res.status(200).send("API IS RUNNING!");
+});
+
+app.use('/',(req, res) => {
+    res.status(200).send("Welcome to Exchange Rate Portal !!");
+});
+
+
+//create server on the port 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ exchange-rates/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const { API_KEY } = require('../constants')
+const axios = require('axios')
+
+router.get('/', (req, res) => {
+    res.status(200).json({ "message": "Exchange rate api check" })
+})
+
+//API TO GET ALL THE EXCHANGE CODES
+router.get('/get-exchange-codes', (req, res) => {
+    axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/codes`)
+        .then(result => res.status(200).json({ result: result.data.supported_codes }))
+        .catch(e => res.status(400).json({ message: "ERROR" }))
+})
+
+//GET EXCHANGE RATES 
+router.post('/get-exchange-rates', (req, res) => {
+    const data = req.body
+    const code = data['code']
+    axios.get(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${code}`)
+        .then(result => res.status(200).json({ result: result.data.conversion_rates }))
+        .catch(e => res.status(400).json({ message: "ERROR" }))
+})
+
+module.exports = router;
